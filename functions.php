09d3138b582ca6e81f723266c6e8fba374d2a93e
kolasyik <mykola.primachenko@gmail.com>
2023-09-29 20:11:22 +0000

    init

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+
+  $parent_style = 'grill-repair';
+
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    [ $parent_style ],
+    //time() //For production use wp_get_theme()->get('Version')
+  );
+
+  wp_enqueue_script(
+    'my-theme-frontend',
+    get_stylesheet_directory_uri() . '/build/index.js',
+    ['wp-element'],
+    //time() //For production use wp_get_theme()->get('Version')
+  );
+
+}
