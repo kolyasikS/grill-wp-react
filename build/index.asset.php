9075e58f5f195c7d14c23287e32dd88f1e64c355
kolasyik <mykola.primachenko@gmail.com>
2023-09-29 13:40:37 +0000

    init commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '85e7ff5947a5289d7812');
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Page from "./page";
+import './main.scss';
+
+ReactDom.render(<Page/>, document.querySelector('#app'));
